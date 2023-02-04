e1118581b589a5dd480d48c408fb67035d8db207
Henrik Hofmeister <henrik@newdawn.dk>
2023-02-04 19:09:04 +0100

    Update React init to use React 18

--- templates/blockware/resource-type-web-page/index.tsx
+++ templates/blockware/resource-type-web-page/index.tsx
@@ -2,12 +2,12 @@
 //
 // GENERATED SOURCE - DO NOT EDIT
 //
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import React from 'react';
-import Root from "./Root";
+import { {{type data.metadata.name}}Page } from "./{{type data.metadata.name}}Page";
 
 const container = document.createElement('div');
 
 document.body.append(container);
 
-ReactDOM.render(<Root />, container);
+createRoot(container).render(<{{type data.metadata.name}}Page />);
--- test/resources/examples/portal/src/browser/pages/my_page/index.tsx
+++ test/resources/examples/portal/src/browser/pages/my_page/index.tsx
@@ -1,12 +1,12 @@
 //
 // GENERATED SOURCE - DO NOT EDIT
 //
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import React from "react";
-import Root from "./Root";
+import { MyPagePage } from "./MyPagePage";
 
 const container = document.createElement("div");
 
 document.body.append(container);
 
-ReactDOM.render(<Root />, container);
+createRoot(container).render(<MyPagePage />);
