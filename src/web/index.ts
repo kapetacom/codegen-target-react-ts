ba06e2d633539c9e2d771f195c1a27f366cb909b
Henrik Hofmeister <henrik@newdawn.dk>
2023-04-10 17:55:40 +0200

    Upgrade to latest interfaces

--- src/web/index.ts
+++ src/web/index.ts
@@ -1,16 +1,17 @@
-import {TargetConfig} from "@kapeta/ui-web-types";
+import {ILanguageTargetProvider} from "@kapeta/ui-web-types";
 
 // @ts-ignore
 const kapetaDefinition = require('../../kapeta.yml');
 const packageJson = require('../../package.json');
 
-const targetConfig : TargetConfig =  {
+const targetConfig : ILanguageTargetProvider =  {
     kind: kapetaDefinition.metadata.name,
     version: packageJson.version,
     title: kapetaDefinition.metadata.title,
     blockKinds:[
         'kapeta/block-type-frontend'
-    ]
+    ],
+    definition: kapetaDefinition
 };
 
 export default targetConfig;
