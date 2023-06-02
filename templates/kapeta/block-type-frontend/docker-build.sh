3bb46623983caab184a552923ac0fad0763da2bd
Henrik Hofmeister <henrik@newdawn.dk>
2023-06-02 09:04:31 +0200

    Add support for running locally using dev containers

--- templates/kapeta/block-type-frontend/docker-build.sh
+++
@@ -1,7 +0,0 @@
-//#FILENAME:scripts/docker-build.sh:write-always:755
-#!/usr/bin/env bash
-
-echo "Building docker image";
-docker build . -t {{lowercase data.metadata.name}} || exit 1
-
-echo "Done. Image ready: {{lowercase data.metadata.name}}";
--- test/resources/examples/portal/scripts/docker-build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Building docker image";
-docker build . -t kapeta/portal || exit 1
-
-echo "Done. Image ready: kapeta/portal";
