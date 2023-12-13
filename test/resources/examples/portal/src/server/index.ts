dc0e78f07bd594db3eebce551c4b7259ae00d3ec
Henrik Hofmeister <henrik@newdawn.dk>
2023-12-13 13:10:00 +0100

    feat: Use V2 of SDK and adjust to match the new API

--- test/resources/examples/portal/src/server/index.ts
+++ test/resources/examples/portal/src/server/index.ts
@@ -1,14 +1,15 @@
-import Path from "path";
-import { Server } from "@kapeta/sdk-server";
-const server = new Server("kapeta/portal", Path.resolve(__dirname, "../.."));
-import { TasksProxyRoute } from "./.generated/proxies/rest/TasksProxyRoute";
-import { SubpageProxyRoute } from "./.generated/proxies/fragments/SubpageProxyRoute";
-
-server.addRoute(new SubpageProxyRoute());
-server.addRoute(new TasksProxyRoute());
+import { ConfigProvider, runApp } from "@kapeta/sdk-config";
+import { createServer } from "./server";
+import { createRoutes } from "./.generated/routes";
+import Path from "node:path";
 
 const BASE_DIR = Path.resolve(__dirname, "../../dist");
-const webpackConfig = require("../../webpack.development.config");
-server.configureAssets(BASE_DIR, webpackConfig);
+runApp(async (configProvider: ConfigProvider) => {
+    const server = await createServer(configProvider);
 
-server.start("web");
+    server.use(await createRoutes(configProvider));
+    const webpackConfig = require("../../webpack.development.config");
+    server.configureAssets(BASE_DIR, webpackConfig);
+
+    server.start("web");
+}, Path.resolve(__dirname, "../../"));
