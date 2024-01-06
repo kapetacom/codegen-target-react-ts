27fbcc6d0a9e8c3df3b62e863183050d21c702ff
Henrik Hofmeister <henrik@newdawn.dk>
2024-01-06 15:05:48 +0100

    feat: Add catch all handler for rest APIs (#54)

--- test/resources/examples/portal/src/server/.generated/routes.ts
+++ test/resources/examples/portal/src/server/.generated/routes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
 import { ConfigProvider } from '@kapeta/sdk-config';
 import { ProxyRouteOptions } from '@kapeta/sdk-proxy-route';
 
@@ -13,6 +13,14 @@ export const createRoutes = async (config: ConfigProvider, opts: ProxyRouteOptio
     restApis.use(await createUsersRouter(config, opts));
 
     restApis.use(await createTasksRouter(config, opts));
+
+    // Catch all unknown routes and return 418 I'm a teapot
+    restApis.all('/*', (req: Request, res: Response, next: NextFunction) => {
+        res.status(418).json({
+            error: 'API endpoint not found',
+        });
+    });
+
     routes.use('/api/rest', restApis);
 
     const fragments = Router();
