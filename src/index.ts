7b9e23b11fa37eb6e1257146be3c6eecf3728ef6
Morten Holm <mohoholm@gmail.com>
2024-06-06 07:35:50 +0200

    Feature/core 2951 add validation (#108)
    * feat: add validation
    
    * fix: promises
    
    * update to latest codegen
    
    * fix: add mui when for AI generated blocks

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,8 @@ import { exec } from '@kapeta/nodejs-process';
 import { mergeDevcontainers } from './target/merge-devcontainers';
 import { mergePackageJson } from './target/merge-package';
 import { addTemplateHelpers, HandleBarsType } from './target/template-helpers';
+import {spawn} from "child_process";
+import {ValidationResult} from "@kapeta/codegen";
 
 export default class ReactTSTarget extends Target {
     constructor(options?: any) {
@@ -40,6 +42,66 @@ export default class ReactTSTarget extends Target {
         }
 
         return super.mergeFile(sourceFile, newFile, lastFile);
+    }
+
+    validate(targetDir: string, files: any[]): Promise<ValidationResult> {
+        console.log('Validating React Typescript project');
+
+        return new Promise((resolve) => {
+            this.runCmd(targetDir, 'npm', ['install'])
+                .then((result) => {
+                    if (!result.valid) {
+                        return result;
+                    }
+                    console.log('npm install completed successfully.');
+                    return this.runCmd(targetDir, 'npm', ['run', 'lint']); // Chain to next step
+                })
+                .then((result) => {
+                    if (!result.valid) {
+                        return result;
+                    }
+                    console.log('npm run lint completed successfully.');
+                    return this.runCmd(targetDir, 'npm', ['run', 'build']); // Chain to next step
+                })
+                .then((result) => {
+                    if (!result.valid) {
+                        resolve(result);
+                        return;
+                    }
+                    console.log("npm run compile completed successfully.");
+                    resolve({ valid: true, error: ''});
+                })
+                .catch(error => resolve(error));
+        });
+    }
+
+    private runCmd(targetDir: string, command: string, args: string[]): Promise<ValidationResult> {
+        return new Promise((resolve, reject) => {
+            const chunks: Buffer[] = [];
+            console.log(`will run ${command}`, args);
+            const childProcess = spawn(command, args, {
+                cwd: targetDir ? targetDir : process.cwd(),
+                shell: true,
+            });
+
+            childProcess.stdout.on('data', (data) => {
+                chunks.push(data);
+            });
+
+            childProcess.stderr.on('data', (data) => {
+                chunks.push(data);
+            });
+
+            childProcess.on('close', (code) => {
+                if (code !== 0) {
+                    console.log(`${command} exited with code ${code}`);
+                    resolve({valid: false, error: Buffer.concat(chunks).toString()});
+                } else {
+                    console.log(`${command} OK`);
+                    resolve({valid: true, error: ''});
+                }
+            });
+        });
     }
 
     async postprocess(targetDir: string, files: GeneratedAsset[]): Promise<void> {
